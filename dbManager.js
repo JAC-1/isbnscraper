fc30906cb88155bfb7e72687e85f2d6978ab0f3e
JAC-1 <79427501+JAC-1@users.noreply.github.com>
2022-11-02 10:14:48 +0900

    Initial add for a cleaner db writer

---
+++ dbManager.js
@@ -0,0 +1,18 @@
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { fileURLToPath } from 'url'
+
+export async function dbReader(jsonDb) {
+   const __dirname = dirname(fileURLToPath(import.meta.url));
+    const file = join(__dirname, jsonDb)
+    const adapter = new JSONFile(file)
+    const db = new Low(adapter)
+
+    await db.read();
+    return db.data
+}
+
+export async function dbWriter(jsonDb) {
+  await db.write()
+
+}
